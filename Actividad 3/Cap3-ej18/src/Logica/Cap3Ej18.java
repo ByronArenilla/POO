f71af3b4ab5fb0c2972d66c09a8bc59c9a0f1a85
Byron Arenilla <barenilla@unal.edu.co>
2024-04-04 10:09:03 -0500

    Avanzo el ejercicio 1

---
+++ Actividad 3/Cap3-ej18/src/Logica/Cap3Ej18.java
@@ -0,0 +1,18 @@
+
+package Logica;
+
+import GUI.IEmpleado;
+
+
+public class Cap3Ej18 {
+
+
+    public static void main(String[] args) {
+        //Llamar a la pantalla
+        IEmpleado pantalla = new IEmpleado();
+        pantalla.setVisible(true);
+        //Poner la pantalla en el medio
+        pantalla.setLocationRelativeTo(null); //Pone la pantalla relativa a nada
+    }
+
+}
---
+++ Actividad 3/Cap3-ej18/src/Logica/Empleado.java
@@ -0,0 +1,14 @@
+
+package Logica;
+
+
+public class Empleado {
+    int codigoEmpledo;
+    String nombres;
+    int horasTrabajadas;
+    int valorHora;
+    int reteFuente;
+    
+    //Pedir cada uno de esos atributos
+    
+}
