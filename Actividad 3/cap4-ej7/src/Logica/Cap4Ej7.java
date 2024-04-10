f15d44284ea6cd37306936cf62c8a0ecbdd386bd
Byron Arenilla <barenilla@unal.edu.co>
2024-04-10 12:47:07 -0500

    Termino el tercer ejercicio

---
+++ Actividad 3/cap4-ej7/src/Logica/Cap4Ej7.java
@@ -0,0 +1,16 @@
+
+package Logica;
+
+import GUI.Pantalla;
+
+
+public class Cap4Ej7 {
+
+
+    public static void main(String[] args) {
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+
+}
