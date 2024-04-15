3543794584c1ecbe5f3c70cee9fefd51528ae650
Byron Arenilla <barenilla@unal.edu.co>
2024-04-15 12:12:13 -0500

    Termino el quinto ejercicio

---
+++ Actividad 3/cap4-ej22/src/Logica/Cap4Ej22.java
@@ -0,0 +1,17 @@
+
+package Logica;
+
+import GUI.Pantalla;
+
+
+public class Cap4Ej22 {
+
+
+    public static void main(String[] args) {
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        
+    }
+
+}
